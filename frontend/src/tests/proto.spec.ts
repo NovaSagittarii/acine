3bbc0cc5b90dc4da995b99db35790efc268159f8
NovaSagittarii <novasagittarii@gmail.com>
2025-02-15 20:17:51 -0700

    setup npm package for protos

---
+++ frontend/src/tests/proto.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest';
+import * as pb from 'acine-proto-dist';
+
+test('pb.Frame create', () => {
+  const f = pb.Frame.create();
+  f.id = 1;
+  f.data = new Uint8Array(4);
+  console.log(f);
+  expect(f.id).toBe(1);
+  expect(f.data.length).toBe(4);
+
+  const dat = pb.Frame.encode(f).finish();
+  const g = pb.Frame.decode(dat);
+  expect(g.id).toBe(f.id);
+  expect(g.data).toStrictEqual(f.data);
+});
