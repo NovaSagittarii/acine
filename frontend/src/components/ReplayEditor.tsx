8331590163d2b3607afa398b3bbe1a193ed68c5f
NovaSagittarii <novasagittarii@gmail.com>
2025-04-09 01:49:09 -0600

    display duration (placeholder) and event count for replay

--- frontend/src/components/ReplayEditor.tsx
+++ frontend/src/components/ReplayEditor.tsx
@@ -11,41 +11,53 @@ export default function ReplayEditor({ replay }: ReplayEditorProps) {
   const [stream, setStream] = useState<null | ReturnType<typeof open>>(null);
 
   return (
-    <div className='py-2 px-4 flex flex-row gap-4 font-sans'>
-      {!isRecording ? (
-        <>
+    <div className='flex flex-col'>
+      <div className='flex flex-row justify-evenly'>
+        <div className='flex flex-row'>
+          {replay.duration} <div className='opacity-50'>ms: duration</div>
+        </div>
+        <div className='flex flex-row'>
+          {replay.events.length} <div className='opacity-50'>: events</div>
+        </div>
+      </div>
+      <div className='py-2 px-4 flex flex-row gap-4 font-sans'>
+        {!isRecording ? (
+          <>
+            <Button
+              className='p-1! w-full bg-black text-white'
+              onClick={() => {
+                setRecording(true);
+                setStream(open());
+              }}
+            >
+              Record
+            </Button>
+            <Button className='p-1! w-full bg-black text-white'>
+              Playback
+            </Button>
+            <Button
+              className='p-1! w-full bg-black text-white'
+              onClick={() => console.log(replay)}
+            >
+              console.log
+            </Button>
+          </>
+        ) : (
           <Button
-            className='p-1! w-full bg-black text-white'
-            onClick={() => {
-              setRecording(true);
-              setStream(open());
+            className='p-1! w-full bg-red-500 text-white'
+            onClick={async () => {
+              setRecording(false);
+              if (stream) {
+                stream.close({ noHover: true });
+                replay.events = await stream.getContents();
+                console.log(replay.events);
+              } else console.error("InputStream wasn't initialized.");
             }}
           >
-            Record
+            Stop Recording
           </Button>
-          <Button className='p-1! w-full bg-black text-white'>Playback</Button>
-          <Button
-            className='p-1! w-full bg-black text-white'
-            onClick={() => console.log(replay)}
-          >
-            console.log
-          </Button>
-        </>
-      ) : (
-        <Button
-          className='p-1! w-full bg-red-500 text-white'
-          onClick={async () => {
-            setRecording(false);
-            if (stream) {
-              stream.close({ noHover: true });
-              replay.events = await stream.getContents();
-              console.log(replay.events);
-            } else console.error("InputStream wasn't initialized.");
-          }}
-        >
-          Stop Recording
-        </Button>
-      )}
+        )}
+      </div>
     </div>
   );
 }
