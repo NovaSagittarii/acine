bde237b6948742faa3ffa21d74ace174166f44d2
NovaSagittarii <novasagittarii@gmail.com>
2025-04-23 02:02:17 -0600

    update frame pb ts test to use string ids; so tests pass

--- proto/dist/ts/frame.spec.ts
+++ proto/dist/ts/frame.spec.ts
@@ -4,10 +4,10 @@ import * as pb from "./index";
 // just make sure the frame exists
 test("pb.Frame create", () => {
   const f = pb.Frame.create();
-  f.id = 1;
+  f.id = "1";
   f.data = new Uint8Array(4);
   console.log(f);
-  expect(f.id).toBe(1);
+  expect(f.id).toBe("1");
   expect(f.data.length).toBe(4);
 
   const dat = pb.Frame.encode(f).finish();
