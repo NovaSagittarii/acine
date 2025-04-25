ded638e88b0952cb15636b9b85eca9031589e2a7
NovaSagittarii <novasagittarii@gmail.com>
2025-04-25 00:42:36 -0600

    add pytest; add basic test on acine/runtime/check.py

---
+++ backend/tests/runtime/test_check.py
@@ -0,0 +1,32 @@
+import pytest
+from acine.runtime.check import Routine, check_once
+from acine_proto_dist.position_pb2 import Rect
+from numpy import random
+from pytest_mock import MockerFixture
+
+
+class TestCheck:
+    def test_check_once_null_condition(self):
+        """
+        Ensure null condition passes through properly.
+        """
+        c = Routine.Condition()
+        assert c.WhichOneof("condition") is None
+        assert check_once(c, None) is True
+
+    @pytest.mark.parametrize("ret", (True, False))
+    def test_check_once_image(self, mocker: MockerFixture, ret: bool):
+        """
+        Ensure check_once calls check_image for image conditions.
+        """
+        c = Routine.Condition(
+            image=Routine.Condition.Image(
+                frame_id="FRAME_ID", tolerance=0.5, regions=[Rect()]
+            )
+        )
+        img = random.rand(2, 3, 4)
+
+        p = mocker.patch("acine.runtime.check.check_image")
+        p.return_value = ret
+        assert check_once(c, img) == ret
+        p.assert_called_once_with(c.image, img)
