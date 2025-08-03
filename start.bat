f89df3c540116bb5b6aa6e351a5ee94f5cede8a3
NovaSagittarii <novasagittarii@gmail.com>
2025-08-03 16:08:18 -0600

    use npx concurrently instead of scuffed batch script to start dev env

--- start.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-:: run together; https://stackoverflow.com/a/33586872
-(
-    start "Acine frontend" frontend\start.bat
-    start "Acine testing environment" testenv\start.bat
-    start "Acine backend" backend\start.bat
-) | set /P "="
