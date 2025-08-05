a95cb213652022523c1fcacf80d7ca6ba8cbd52f
NovaSagittarii <novasagittarii@gmail.com>
2025-08-05 01:29:10 -0600

    fix f.id type on frontend proto.spec.ts

--- frontend/src/tests/proto.spec.ts
+++ frontend/src/tests/proto.spec.ts
@@ -3,10 +3,10 @@ import * as pb from 'acine-proto-dist';
 
 test('pb.Frame create', () => {
   const f = pb.Frame.create();
-  f.id = 1;
+  f.id = '1';
   f.data = new Uint8Array(4);
-  console.log(f);
-  expect(f.id).toBe(1);
+  // console.log(f);
+  expect(f.id).toBe('1');
   expect(f.data.length).toBe(4);
 
   const dat = pb.Frame.encode(f).finish();
