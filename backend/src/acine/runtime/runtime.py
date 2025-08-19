c5c698df543cf0d65c4dff145b4d04df81fa4c46
NovaSagittarii <novasagittarii@gmail.com>
2025-08-18 22:55:12 -0600

    trigger on_pop event properly

--- backend/src/acine/runtime/runtime.py
+++ backend/src/acine/runtime/runtime.py
@@ -141,7 +141,7 @@ class Runtime:
             # handle pop stack (return nodes)
             # note: type=RETURN nodes have no fixed edges!
             if self.context.curr.type & Routine.Node.NODE_TYPE_RETURN:
-                self.context.curr = self.context.return_stack.pop()
+                self.context.curr = self.pop()
                 continue
 
             # is deepcopy needed?
