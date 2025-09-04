6bf7689d592b568b4b74a37a9a419e01eadb3b1f
NovaSagittarii <novasagittarii@gmail.com>
2025-09-04 17:30:21 -0600

    display nodes sorted by id

--- frontend/src/client/util.ts
+++ frontend/src/client/util.ts
@@ -58,3 +58,14 @@ export function toOutCoordinates(
 
   return { x, y };
 }
+
+/**
+ * Utility function used in .sort() as
+ *
+ * .sort((a, b) => compare(a, b))
+ */
+export function compare<T>(a: T, b: T): number {
+  if (a < b) return -1;
+  else if (a == b) return 0;
+  else return 1;
+}
--- frontend/src/components/NodeList.tsx
+++ frontend/src/components/NodeList.tsx
@@ -6,6 +6,7 @@ import Node from '@/components/Node';
 import useForceUpdate from './useForceUpdate';
 import { useEffect } from 'react';
 import { NodePreset } from './Node.util';
+import { compare } from '../client/util';
 
 /**
  * Editor for nodes & transitions of the DFA.
@@ -28,22 +29,24 @@ export default function NodeList() {
     <div className='w-full h-full pb-4 overflow-hidden flex flex-col gap-4'>
       <div className='h-full overflow-y-scroll'>
         {Object.values(routine.nodes).length === 0 && 'No nodes yet.'}
-        {Object.values(routine.nodes).map((node) => (
-          <div
-            id={node.id}
-            key={node.id}
-            className='relative'
-            onClick={() => $selectedNode.set(node)}
-          >
-            <Node node={node} selected={node == selectedNode} />
-            <CloseButton
-              onClick={() => {
-                delete routine.nodes[node.id];
-                forceUpdate();
-              }}
-            />
-          </div>
-        ))}
+        {Object.values(routine.nodes)
+          .sort((a, b) => compare(a.id, b.id))
+          .map((node) => (
+            <div
+              id={node.id}
+              key={node.id}
+              className='relative'
+              onClick={() => $selectedNode.set(node)}
+            >
+              <Node node={node} selected={node == selectedNode} />
+              <CloseButton
+                onClick={() => {
+                  delete routine.nodes[node.id];
+                  forceUpdate();
+                }}
+              />
+            </div>
+          ))}
       </div>
       <Button
         className='bg-black text-white'
