d5701409775682ba52a11e2a22d3809abf9a3922
NovaSagittarii <novasagittarii@gmail.com>
2025-12-22 18:57:57 -0700

    implement in-memory py7zr write (skip write to file before archive)

--- backend/src/acine/persist.py
+++ backend/src/acine/persist.py
@@ -3,11 +3,13 @@ Image persistence / save to disk.
 """
 
 import os
+from io import BytesIO
 from pathlib import Path
 from typing import Final, List, Optional
 
 import py7zr
 from aiofiles import open as aopen
+from py7zr.helpers import ArchiveTimestamp
 
 DIRNAME: Final[str] = os.path.dirname(__file__)
 PATH: Final[str] = os.path.join(DIRNAME, "..", "..", "data")
@@ -58,6 +60,16 @@ async def fs_read(filename: List[str]) -> bytes:
 def mkdir(dirpath: List[str]) -> None:
     """ensure folder exists"""
     os.makedirs(resolve(*dirpath), exist_ok=True)
+
+
+class BytesData:
+    """Used for input to py7zr.Worker.writestr"""
+
+    def __init__(self, data: bytes):
+        self.__data = BytesIO(data)
+
+    def data(self) -> BytesIO:
+        return self.__data
 
 
 class OutputStream(py7zr.io.Py7zIO):
@@ -125,14 +137,44 @@ class PrefixedFilesystem:
         return await fs_write([*self.prefix, *filename], contents)
 
     async def write_archive(self, filename: List[str], contents: bytes) -> None:
-        mkdir([*self.prefix, "tmp"])
-        os.chdir(self.resolve("tmp"))
-        path = Path(*filename)
-        async with aopen(path, "wb") as file:
-            await file.write(contents)
         with py7zr.SevenZipFile(Path(self.resolve("archive.7z")), "a") as archive:
-            archive.write(path)
-        os.remove(path)
+            # py7zr.SevenZipFile.write()
+            folder = archive.header.initialize()
+            path = Path(*filename)
+            # py7zr.SevenZipFile._make_file_info() -- but the file doesn't exist
+            # so make up something reasonable, everything should be fine, but
+            # "attributes" and "filename" might be incorrect on non-win32 OS
+            file_info = {
+                "origin": path,
+                "filename": str(path),
+                "emptystream": False,
+                "attributes": 32,  # some attribute flags, copied from Windows 11 file
+                "uncompressed": len(contents),
+                "creationtime": ArchiveTimestamp.from_now(),
+                "lastwritetime": ArchiveTimestamp.from_now(),
+                "lastaccesstime": ArchiveTimestamp.from_now(),
+            }
+            archive.header.files_info.files.append(file_info)
+            archive.header.files_info.emptyfiles.append(file_info["emptystream"])
+            archive.files.append(file_info)
+
+            # py7zr.Worker.archive()
+            worker = archive.worker
+            current_file_index = worker.current_file_index
+            foutsize, crc = worker.writestr(archive.fp, BytesData(contents), folder)
+            worker.header.files_info.files[current_file_index]["maxsize"] = foutsize
+            worker.header.files_info.files[current_file_index]["digest"] = crc
+            worker.last_file_index = current_file_index
+            worker.current_file_index += 1
+
+        # mkdir([*self.prefix, "tmp"])
+        # os.chdir(self.resolve("tmp"))
+        # path = Path(*filename)
+        # async with aopen(path, "wb") as file:
+        #     await file.write(contents)
+        # with py7zr.SevenZipFile(Path(self.resolve("archive.7z")), "a") as archive:
+        #     archive.write(path)
+        # os.remove(path)
 
     async def read(self, filename: List[str]) -> bytes:
         return await fs_read([*self.prefix, *filename])
