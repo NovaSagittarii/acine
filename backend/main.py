15ec5f4814e37e6688004d00d965315b436626d2
NovaSagittarii <novasagittarii@gmail.com>
2025-12-29 12:52:30 -0800

    simplify server main loop

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from autobahn.asyncio.websocket import WebSocketServerFactory  # type: ignore
 from acine.instance_manager import EXAMPLE_ID, Routine, create_testenv, validate_routine
 from acine.server import AcineServerProtocol
 
-if __name__ == "__main__":
+async def main():
     # make sure Example Routine (testenv) exists
     if not validate_routine(Routine(id=EXAMPLE_ID)):
         create_testenv()
@@ -13,15 +13,11 @@ if __name__ == "__main__":
     factory = WebSocketServerFactory()
     factory.protocol = AcineServerProtocol
 
-    loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, "127.0.0.1", 9000)
-    server = loop.run_until_complete(coro)
-
+    loop = asyncio.get_running_loop()
+    server = await loop.create_server(factory, "127.0.0.1", 9000)
+    
     print("starting")
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        server.close()
-        loop.close()
+    await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
