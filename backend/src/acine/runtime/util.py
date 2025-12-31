43674395be9611e914608971a17c0f339a889722
NovaSagittarii <novasagittarii@gmail.com>
2025-12-31 16:21:36 -0700

    add intertask communication utility class
    used in the 2-threaded implementation of runtime where there is
    a MainThread that handles web requests and makes goto calls, and
    a Worker thread that services those goto calls, the Worker thread
    uses Python recursion to implement subroutines.

--- backend/src/acine/runtime/util.py
+++ backend/src/acine/runtime/util.py
@@ -7,11 +7,15 @@ Currently only contains time (sleep/now) functions.
 import asyncio
 import time
 from functools import lru_cache
+from typing import Generic, TypeVar
 
 import cv2
 
 from acine.persist import resolve
 from acine.runtime.check_image import ImageBmpType
+
+T = TypeVar("T")
+U = TypeVar("U")
 
 
 def now() -> int:
@@ -40,3 +44,33 @@ def get_frame(routine_id: str, frame_id: str) -> ImageBmpType:
     assert frame_id, "frame_id not set"
     path = resolve(routine_id, "img", f"{frame_id}.png")  # probably in BGR
     return cv2.imread(path)  # type: ignore
+
+
+class IntertaskProcedure(Generic[T, U]):
+    """
+    For procedures called by one coroutine to be done by a separate coroutine.
+    """
+
+    def __init__(self) -> None:
+        self.input = asyncio.Queue[T]()
+        self.output = asyncio.Queue[U]()
+        self.waiting: int = 0
+
+    def __del__(self) -> None:
+        assert not self.waiting
+
+    async def call(self, input: T) -> U:
+        """From calling side."""
+        self.waiting += 1
+        await self.input.put(input)
+        return await self.output.get()
+
+    async def await_call(self) -> T:
+        """From implementation side."""
+        return await self.input.get()
+
+    async def resolve(self, output: U) -> None:
+        """From implementation side."""
+        assert self.waiting
+        self.waiting -= 1
+        return await self.output.put(output)
