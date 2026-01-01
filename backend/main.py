ba5cfeb9ef5f1d1b40ea87d16523352c6a3eb17e
NovaSagittarii <novasagittarii@gmail.com>
2026-01-01 15:47:28 -0700

    fix whitespace
    oops i think i edited it without precommit on the desktop

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ from autobahn.asyncio.websocket import WebSocketServerFactory  # type: ignore
 from acine.instance_manager import EXAMPLE_ID, Routine, create_testenv, validate_routine
 from acine.server import AcineServerProtocol
 
-async def main():
+
+async def main() -> None:
     # make sure Example Routine (testenv) exists
     if not validate_routine(Routine(id=EXAMPLE_ID)):
         create_testenv()
@@ -15,9 +16,10 @@ async def main():
 
     loop = asyncio.get_running_loop()
     server = await loop.create_server(factory, "127.0.0.1", 9000)
-    
+
     print("starting")
     await server.serve_forever()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
