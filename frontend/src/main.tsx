81b5a577b26d0a5ac898861d7419d9953ecce059
NovaSagittarii <novasagittarii@gmail.com>
2026-01-02 15:03:07 -0700

    fix lint rule react-refresh/only-export-components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App.tsx';
 import { useSetupShortcuts } from './components/useShortcut.ts';
 
-function AppSetup() {
+export function AppSetup() {
+  // I'm pretty sure you don't need export but there's the lint rule
+  // react-refresh/only-export-components since fast refresh only works when
+  // a file has exports (though this probably won't be edited?)
   useSetupShortcuts();
   return <></>;
 }
