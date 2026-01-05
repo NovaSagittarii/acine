dc368e359e4874a9381b53a4f20fc3e12a03f298
NovaSagittarii <novasagittarii@gmail.com>
2026-01-04 20:05:43 -0700

    add auto-retry for image lookup
    image might not be pushed to server yet

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,12 @@ Maybe migrate main server sometime?
 https://github.com/pallets/quart/?tab=readme-ov-file#quickstart
 """
 
+import asyncio
 import io
 import os
 import re
 from pathlib import Path
-from typing import Final
+from typing import Any, Final
 
 from quart import Quart, Response, send_file
 from quart_cors import cors
@@ -26,11 +27,46 @@ app = cors(app, allow_origin=CORS)
 # app = cors(app, allow_origin='*')  # if remote access
 
 
+def retry(times: int, exceptions: tuple[Exception]) -> Any:
+    """
+    Retry Decorator
+    Retries the wrapped function/method `times` times if the exceptions listed
+    in ``exceptions`` are thrown
+    :param times: The number of times to repeat the wrapped function/method
+    :type times: Int
+    :param Exceptions: Lists of exceptions that trigger a retry attempt
+    :type Exceptions: Tuple of Exceptions
+
+    from: https://stackoverflow.com/a/64030200
+    """
+
+    def decorator(func):
+        async def newfn(*args, **kwargs):
+            attempt = 0
+            while attempt < times:
+                try:
+                    return await func(*args, **kwargs)
+                except exceptions:
+                    print(
+                        "Exception thrown when attempting to run %s, attempt "
+                        "%d of %d" % (func, attempt, times)
+                    )
+                    attempt += 1
+                    await asyncio.sleep(1)
+            return func(*args, **kwargs)
+
+        return newfn
+
+    return decorator
+
+
+@retry(3, (FileNotFoundError,))
 @app.route("/data/<routine_id>/img/<img_id>")
 async def read_img(routine_id: str, img_id: str) -> Response:
     return await send_file(Path(DATA_PATH, routine_id, "img", f"{img_id}.png"))
 
 
+@retry(3, (FileNotFoundError,))
 @app.route("/data/<routine_id>/archive/<img_id>")
 async def read_img_archive(routine_id: str, img_id: str) -> Response:
     data = await PrefixedFilesystem([routine_id]).read_archive([f"{img_id}.bmp"])
