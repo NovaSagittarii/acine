09bdcc97ee704586833f00226c2824795002a509
NovaSagittarii <novasagittarii@gmail.com>
2026-01-04 18:48:41 -0700

    simplify some shortcut key names A-Z0-9, [], `, esc, del, enter

--- frontend/src/components/ShortcutBindingsDisplay.tsx
+++ frontend/src/components/ShortcutBindingsDisplay.tsx
@@ -1,5 +1,20 @@
 import { useStore } from '@nanostores/react';
-import { $bindings } from './useShortcut';
+import { $bindings, KeyCode } from './useShortcut';
+
+const KeycodeDisplay: Partial<Record<KeyCode, string>> = {
+  Backquote: '`',
+  BracketLeft: '[',
+  BracketRight: ']',
+  Escape: 'ESC',
+  Delete: 'DEL',
+  Enter: '↵',
+};
+
+function renderKeycode(key: KeyCode): string {
+  if (key.startsWith('Key')) return key.substring(3);
+  if (key.startsWith('Digit')) return key.substring(5);
+  return KeycodeDisplay[key] || key;
+}
 
 export default function ShortcutBindingsDisplay() {
   const bindings = useStore($bindings);
@@ -11,9 +26,10 @@ export default function ShortcutBindingsDisplay() {
         .map(([k, v]) => {
           if (!v) return <></>;
           const { label } = v;
+          k = renderKeycode(k as KeyCode);
           return (
             <div className='flex gap-2 text-xs' key={k}>
-              <div className='font-mono min-w-4 min-h-4 border border-black rounded-sm'>
+              <div className='font-mono min-w-4 min-h-4 px-1 border border-black rounded-sm text-center'>
                 {k}
               </div>
               <div>{label}</div>
