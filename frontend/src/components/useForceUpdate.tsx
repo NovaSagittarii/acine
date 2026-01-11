fbb6ac5ed3e83dd1d32b3eb664d36a63eb77b951
NovaSagittarii <novasagittarii@gmail.com>
2026-01-11 15:30:53 -0700

    use useReducer instead of useEffect for forceUpdate

--- frontend/src/components/useForceUpdate.tsx
+++ frontend/src/components/useForceUpdate.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
 // from https://stackoverflow.com/a/53837442
+// https://legacy.reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
 export default function useForceUpdate() {
-  const [, setValue] = useState(0);
-  return () => setValue((value) => value + 1);
+  const [_, forceUpdate] = useReducer((x) => x + 1, 0);
+  return forceUpdate;
 }
